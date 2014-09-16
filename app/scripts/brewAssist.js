3cf4d865d9761b7842150d54f73b719fa81e36ea
daniel <demise4u@gmail.com>
2014-09-16 17:03:58 -0500

    initial commit

---
+++ app/scripts/brewAssist.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('brewAssist', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngResource', 'ngRoute'])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'partials/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  })
+;
